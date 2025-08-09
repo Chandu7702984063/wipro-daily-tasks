9699e4addae9030fee2c89ea5de325ade032d401
chandrasekhar git <chandrasekharreddyram@gmail.com>
2025-08-09 11:38:33 +0530

    ex2

---
+++ code/2025-08-09/Ex2/kafka-consumer/src/main/java/com/example/kafka_consumer/listener/TopicListener.java
@@ -0,0 +1,13 @@
+package com.example.kafka_consumer.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TopicListener {
+
+    @KafkaListener(topics = "learn-subject", groupId = "spring-consumer-group")
+    public void consume(String message) {
+        System.out.println("Consumer received: " + message);
+    }
+}
